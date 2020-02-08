612aefda9f3b91955f83de3e8770cab889503a7f
pragmatic-distribution-6705 <philipmkatz@gmail.com>
2020-02-08 02:09:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ function yourAnimal(animal) {
 }
 
 function add2(n) {
-  return n + 2
+  return n + 2;
 
   // Feel free to move things around!
   const two = 2
 }
 
-var funkyFunction = "FUNKY!";
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
 
+var theFunk = "FUNKY!"
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
